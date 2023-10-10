c6137ad7ef0b97efb9de83bf6ab8125d29324b79
Keks <keks@htmlacademy.ru>
2023-10-11 01:11:02 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/functions.js
@@ -0,0 +1,17 @@
+
+const checkStringLength = (string, maxLength) => {
+  const result = string.length <= maxLength;
+  return result;
+};
+
+
+const checkString = (palindrom) => {
+  const normalazeString = palindrom.toLowerCase().replaceAll(' ','');
+  let newStringReverse = '';
+  for (let i = normalazeString.length - 1; i >= 0; i--) {
+    newStringReverse += normalazeString[i];
+  }
+  const result = normalazeString === newStringReverse;
+  return result;
+};
+
